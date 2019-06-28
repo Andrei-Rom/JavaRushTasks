10b641303c358cdb0bdfb2a09dfb006f6b8100b3
Andrei Rom <andrisrom@gmail.com>
2019-06-28 22:24:23 +0300

    task(8-10)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,53 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public void setPoint(double x, double y, char c){
+        int intX = (int) Math.round(x);
+        int intY = (int) Math.round(y);
+        if (intX >= 0 || intY >= 0 || intY < matrix.length || intX < matrix[0].length) matrix[intY][intX] = c;
+
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int iy = 0; iy < matrix.length; iy++) {
+            for (int jx = 0; jx < matrix[0].length; jx++) {
+                if (matrix[iy][jx] != 0) setPoint(x+jx, y+iy, c);
+            }
+        }
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
 }
