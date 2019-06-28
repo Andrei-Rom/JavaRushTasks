f5936f6bc3c3261c51f7510cc1e66f2608b0e5f1
Andrei Rom <andrisrom@gmail.com>
2019-06-28 23:46:55 +0300

    Arkanoid is done

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,56 +1,35 @@
 package com.javarush.task.task24.task2413;
 
-public class Brick extends BaseObject  {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
-    }
+/**
+ * Класс для объекта "кирпич".
+ */
+public class Brick extends BaseObject {
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
 
     public Brick(double x, double y) {
         super(x, y, 3);
     }
 
+    /**
+     * Рисуем себя на холсте
+     */
     @Override
-    public void draw(Canvas canvas) {
-
+    void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'H');
     }
 
+    /**
+     * Ничего не делаем - кирпич неподвижен
+     */
     @Override
-    public void move() {
-
-    }
-
-    @Override
-    public boolean isIntersec(BaseObject o) {
-        return super.isIntersec(o);
-    }
-
-    @Override
-    public double getX() {
-        return super.getX();
-    }
-
-    @Override
-    public double getY() {
-        return super.getY();
-    }
-
-    @Override
-    public double getRadius() {
-        return super.getRadius();
-    }
-
-    @Override
-    public void setX(double x) {
-        super.setX(x);
-    }
-
-    @Override
-    public void setY(double y) {
-        super.setY(y);
-    }
-
-    @Override
-    public void setRadius(double radius) {
-        super.setRadius(radius);
+    void move() {
+        //do nothing
     }
 }
