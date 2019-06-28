a9be6c39fc42e69f3cb55f3873bd7eea128bb607
Andrei Rom <andrisrom@gmail.com>
2019-06-28 21:19:54 +0300

    task(3-5)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,11 +1,27 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     public static void main(String[] args) {
     }
 
+    private Ball ball;
+    private List<Brick> bricks;
+    private Stand stand;
+    public static Arkanoid game;
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     private int width;
     private int height;
+
+    public void run(){}
+
+    public void move(){}
 
     public int getWidth() {
         return width;
@@ -15,11 +31,35 @@ public class Arkanoid {
         return height;
     }
 
+    public Ball getBall() {
+        return ball;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
     public void setWidth(int width) {
         this.width = width;
     }
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
     }
 }
