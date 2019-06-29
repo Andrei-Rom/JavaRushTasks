b0626e8ac11f7dca8d9cd9cefe5c65654727aed4
Andrei Rom <andrisrom@gmail.com>
2019-06-29 21:04:45 +0300

    commit

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void printHello(){
+        System.out.println("Hello");
+    }
+
+    public void printGoodbye(){
+        System.out.println("Goodbye!");
+    }
+}
