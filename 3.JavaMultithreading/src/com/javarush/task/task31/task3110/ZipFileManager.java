3a50091e547d8386a72b5b09c99b3e1d4516fbba
Andrei Rom <andrisrom@gmail.com>
2019-07-12 17:09:10 +0300

    Task 2

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,6 +14,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            byte[] buff = new byte[1000];
+            while (inputStream.available() != -1) {
+                int count = inputStream.read(buff);
+                zipOutputStream.write(buff, 0, count);
+            }
+        }
+
 
     }
 }
