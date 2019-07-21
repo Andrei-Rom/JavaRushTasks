5cc81e7f3a8dbb9457c2e8640efa9e9feabcac8b
Andrei Rom <andrisrom@gmail.com>
2019-07-21 21:59:25 +0300

    solution of skipped task2209 task3012

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,38 +1,66 @@
 package com.javarush.task.task22.task2209;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /*
 Составить цепочку слов
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
         //...
-        String fileName = null;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)))
-        {fileName = reader.readLine();} catch (IOException e){ e.printStackTrace();}
-
-        String content = null;
-        try {
-            content = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        while (reader.ready())
+        {
+            String[] s = reader.readLine().split("\\s");
+            Collections.addAll(list, s);
         }
-        String[] words = content.split(" ");
-
-
+        reader.close();
+        String[] words = new String[list.size()];
+        words = list.toArray(words);
         StringBuilder result = getLine(words);
         System.out.println(result.toString());
-
+        scanner.close();
     }
 
+    public static StringBuilder getLine(String... words)
+    {
+        ArrayList<String> strings  = new ArrayList<>();
+        Collections.addAll(strings, words);
+        StringBuilder sb = new StringBuilder();
+        if (strings.size() == 0)
+            return new StringBuilder();
 
-    public static StringBuilder getLine(String... words) {
-          return null;
+        sb.append(strings.get(0));
+        strings.remove(0);
+
+        while (strings.size()>0){
+            for (int i = 0; i < strings.size(); i++) {
+                String a = strings.get(i).toUpperCase().toLowerCase();
+                String b = sb.toString().toUpperCase().toLowerCase();
+                if (a.charAt(0) == b.charAt(sb.length() - 1))
+                { // в конец
+                    sb.append(" ").append(strings.get(i));
+                    strings.remove(i);
+                    continue;
+                }
+
+                if (b.charAt(0) == a.charAt(a.length() - 1))
+                { //в начало
+                    sb.insert(0, " ");
+                    sb.insert(0, strings.get(i));
+                    strings.remove(i);
+                }
+            }
+        }
+        return sb;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -7,10 +7,18 @@ package com.javarush.task.task30.task3012;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.createExpression(74);
+        solution.createExpression(2);
     }
 
     public void createExpression(int number) {
         //напишите тут ваш код
+        // 1, 3, 9, 27, 81, 243, 729, 2187
+        StringBuilder result = new StringBuilder();
+        result.append(number).append(" =");
+        String[] formats = {"", " + %d", " - %d"};
+        for (int i = number, tri = 1; 0 < i; i = ++i / 3, tri *= 3) {
+            result.append(String.format(formats[i % 3], tri));
+        }
+        System.out.println(result);
     }
 }
