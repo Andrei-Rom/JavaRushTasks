487009ff0423f1173a168457600ce5a8fb2785e9
Andrei Rom <andrisrom@gmail.com>
2019-07-21 22:40:26 +0300

    task2213 - 1-7

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -2,6 +2,25 @@ package com.javarush.task.task22.task2213;
 
 public class Tetris {
     public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
+    private Field field;
+    private Figure figure;
+    static Tetris game;
 
+    public void run() {
+
+    }
+    public void step(){
+
+    }
+    
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
     }
 }
