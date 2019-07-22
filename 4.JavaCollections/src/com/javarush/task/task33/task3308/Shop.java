8810620e02d2a09343d210a9900ba5558b1f8d57
Andrei Rom <andrisrom@gmail.com>
2019-07-22 17:21:00 +0300

    task3308 task3309

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+
+@XmlType(name = "shop")
+@XmlRootElement
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    @XmlAnyElement
+    public String[] secretData;
+
+    @XmlType(name = "goods")
+    @XmlRootElement
+    public static class Goods {
+        @XmlAnyElement
+        List<String> names;
+    }
+}
