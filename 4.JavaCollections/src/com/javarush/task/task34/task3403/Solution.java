f3f2414431ac345b2ca71bb0d2d9ef49bae8584d
Andrei Rom <andrisrom@gmail.com>
2019-07-24 20:36:41 +0300

    commit

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task34.task3403;
+
+/* 
+Разложение на множители с помощью рекурсии
+*/
+public class Solution {
+    public void recurse(int n) {
+        for (int i = 2; i <= n; i++) {
+            if ((n % i) == 0){
+                if (n != i){
+                    System.out.print(i + " ");
+                    recurse(n / i);
+                }
+                else{
+                    System.out.print(i + " ");
+                }
+                break;
+            }
+        }}
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recurse(132);
+    }
+}
