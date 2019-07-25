c07513d6ce8c15397783b656ee1da3c4c0b46cbf
Andrei Rom <andrisrom@gmail.com>
2019-07-25 19:25:53 +0300

    task3008 (6)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,28 @@
 package com.javarush.task.task30.task3008;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-        
+        ConsoleHelper.writeMessage("Введи порт сервера");
+
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("сервер запущен");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("ОШИБКА!");
+        }
+
+    }
+
+    private static class Handler extends Thread {
+        Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
