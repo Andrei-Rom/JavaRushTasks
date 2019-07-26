528f760f9cbf927fa7d9ca20fd16270684ec8266
Andrei Rom <andrisrom@gmail.com>
2019-07-26 18:40:08 +0300

    task3702 (4)

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -1,11 +1,10 @@
 package com.javarush.task.task37.task3702.male;
 
+import com.javarush.task.task37.task3702.AbstractFactory;
 import com.javarush.task.task37.task3702.Human;
 import com.javarush.task.task37.task3702.male.KidBoy;
 
-public class MaleFactory<T extends Human> {
-
-
+public class MaleFactory implements AbstractFactory {
     public Human getPerson (int age){
 
         if (age > 19) return new Man();
