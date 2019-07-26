c323f9aaf623264dcfb9ad1fb9bd6157c164502b
Andrei Rom <andrisrom@gmail.com>
2019-07-26 23:15:53 +0300

    task 2712 (9)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -28,4 +28,18 @@ public class Advertisement {
     public long getAmountPerOneDisplaying() {
         return amountPerOneDisplaying;
     }
+
+    public void revalidate() {
+        if (hits <= 0) throw new UnsupportedOperationException();
+        else hits--;
+    }
+
+    @Override
+    public String toString() {
+        return name + " is displaying... " + getAmountPerOneDisplaying() + ", " + getAmountPerOneDisplaying()/duration*1000;
+    }
+
+    public int getHits() {
+        return hits;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task27.task2712.ad;
 
-import com.javarush.task.task27.task2712.ConsoleHelper;
+import java.util.List;
 
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
@@ -10,7 +10,8 @@ public class AdvertisementManager {
         this.timeSeconds = timeSeconds;
     }
 
-    public void processVideos(){
-        ConsoleHelper.writeMessage("calling processVideos method");
+    public void processVideos() {
+        List<Advertisement> availableVideos = storage.list();
+        if (availableVideos.isEmpty()) throw new NoVideoAvailableException();
     }
 }
