41f88a35415e3876e7557fa988ac73090f509b1b
Andrei Rom <andrisrom@gmail.com>
2019-07-27 13:10:56 +0300

    task (11-12)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+
+public class StatisticManager {
+    private static volatile StatisticManager instance;
+
+    private StatisticManager() {
+    }
+
+    public static StatisticManager getInstance() {
+        if (instance == null) {
+            instance = new StatisticManager();
+        }
+        return instance;
+    }
+
+    public void register(EventDataRow data) {}
+}
